aebfbffe362d4e59f66e6420f46b042c83f90ff1
Antoni Moszynski <antmoszyns@gmail.com>
2023-12-12 12:32:54 +0100

    add threshold filtering

--- masserstein/nmr_spectrum.py
+++ masserstein/nmr_spectrum.py
@@ -13,10 +13,15 @@ from warnings import warn
 
 from masserstein import Spectrum
 
+
 class NMRSpectrum(Spectrum):
-    def __init__(self, formula='', threshold=0.001, total_prob=None,
+    def __init__(self, intensities_threshold=0, formula='', threshold=0.001, total_prob=None,
                  charge=1, adduct=None, confs=None, label=None, protons=None, **other):
-        
+        if confs is not None:
+            confs = np.array(confs)
+            if len(confs.shape) != 2 or confs.shape[1] != 2:
+                raise ValueError("intensities are not properly shaped")
+            confs = confs[confs[:, 1] > intensities_threshold].tolist()
         Spectrum.__init__(self, formula=formula, threshold=threshold, total_prob=total_prob,
-                 charge=charge, adduct=adduct, confs=confs, label=label, **other)
+                          charge=charge, adduct=adduct, confs=confs, label=label, **other)
         self.protons = protons
